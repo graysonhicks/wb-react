ec85e29e17ca9754923591d63ac150747832acea
Grayson Hicks <graysonhicks@gmail.com>
2018-03-28 11:33:27 -0400

    refactor for stateless functional

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Inventory extends Component {
-    render(){
-        return (
-            <div className="inventory">
-                Inventory!
-            </div>
-        )
-    }
+  render() {
+    return <div className="inventory">Inventory!</div>;
+  }
 }
 
 export default Inventory;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import App from './components/App';
+import App from "./components/App";
 
 import "./css/style.css";
 
-render(<App/>, document.getElementById('main'));
+render(<App />, document.getElementById("main"));
