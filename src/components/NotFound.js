490a560aed573a8e25af834ee73cf46390d3da0e
Grayson Hicks <graysonhicks@gmail.com>
2018-03-28 13:59:26 -0400

    basic router

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFound = props => (
+  <div>
+    <h2>Sorry! Nothing here!</h2>
+  </div>
+);
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-import App from "./components/App";
+import Router from "./components/Router";
 
 import "./css/style.css";
 
-render(<App />, document.getElementById("main"));
+render(<Router />, document.getElementById("main"));
