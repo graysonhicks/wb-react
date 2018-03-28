6507a3c2c3919c4398689b3f96ebc8f7c2315977
Grayson Hicks <graysonhicks@gmail.com>
2018-03-28 10:41:34 -0400

    basic components and deploy command

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-// let's go!
-alert("hey")
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import App from './components/App';
+
+import "./css/style.css";
+
+render(<App/>, document.getElementById('main'));
